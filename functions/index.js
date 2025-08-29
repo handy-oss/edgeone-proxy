dd69c8dbd3ef3221ed6cd2ceb8f535247145782d
zhx <123@163.com>
2025-08-29 17:51:57 +0800

    路径

---
+++ functions/index.js
@@ -0,0 +1,7 @@
+/**
+ * This is the final, correct version of the proxy function.
+ * It leverages a professional third-party proxy to handle anti-bot measures.
+ */
+export async function onRequest(context) {
+    return;
+}
